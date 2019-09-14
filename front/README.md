2139372990989d1233a9db78d5698e2355d301c3
iovelasco <ioce.velasco@mercadolibre.com>
2019-09-14 15:04:24 -0300

    Teste de subida de componentes

---
+++ front/README.md
@@ -0,0 +1,6 @@
+# 4biz
+
+Instruciones para levantar entorno
+
+* npm install
+* npm start
---
+++ front/src/app.js
@@ -0,0 +1,3 @@
+import "./styles/main.scss";
+import "./js/bin/materialize.min.js";
+import "./static/logo.png";
